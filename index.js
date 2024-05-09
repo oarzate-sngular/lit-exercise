5ef399d82c9d71da8afa69909ddf02e7bded3369
Osvaldo Arzate Santiago <osvaldo.arzate.contractor@bbva.com>
2024-05-08 22:23:21 -0600

    add example get

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
-import { LitElement, html } from "lit-element";
+import { LitElement, css, html } from "lit-element";
 
 class HomePage extends LitElement {
   static get styles() {
-    return []
+    return css`
+      p {
+        color: black
+      }
+    `
+  }
+
+  async getUser() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const users = await response.json()
+
+    this.users = users
+  }
+
+  firstUpdated() {
+    this.getUser()
+    console.log("3. FIRST UPDATED")
+  }
+
+  updated() {
+    console.log("4. UPDATED")
   }
 
   static get properties() {
-    return {}
+    return {
+      users: { type: Array }
+    }
   }
 
   constructor() {
+    console.log("1. CONSTRUCTOR")
     super()
+    this.users = []
   }
 
   render() {
+    console.log("2. RENDER", this.user)
     return html`
-      <p>Home page</p>
+      <p>Home pages</p>
+      ${this.users.map((user) => html`
+        <p>${user.name}</p>
+      `)}
     `
   }
 }
